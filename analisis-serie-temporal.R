a8bfee2a1b3030500c0bdb7e2104b3b367ea106b
wparedesgt <wparedesgt@gmail.com>
2018-07-01 11:07:52 -0600

    plots

--- analisis-serie-temporal.R
+++ analisis-serie-temporal.R
@@ -1,2 +1,26 @@
 #Analisis serie temporal
+library(tidyverse)
+library(ggthemes)
+
+
+#Cargando los datos bajados de https://finance.yahoo.com/ 
+
+AMZN <- read.csv("data/AMZN.csv")
+GOOG <- read.csv("data/GOOG.csv")
+
+#Convirtiendo los data set de fecha
+
+AMZN$Date <- as.Date(AMZN$Date)
+GOOG$Date <- as.Date(GOOG$Date)
+
+AMZN %>% ggplot(aes(Date, Close)) + geom_line(aes(color="amazon")) +
+  geom_line(data=GOOG,aes(color="google")) +
+  labs(color="Legend") +
+  scale_colour_manual("", breaks = c("amazon","google"),
+                      values = c("blue", "brown")) +
+  ggtitle("Precios de cierre de las acciones: Amazon and Google ") + 
+  theme(plot.title = element_text(lineheight=.7, face="bold"))
+
+ggsave("plots/cierre-google-amazon.png")
+
 
